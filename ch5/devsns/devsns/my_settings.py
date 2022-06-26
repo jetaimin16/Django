0d1ec79c966f2151adcdc882505bfcf06ac0a8b0
jetaimin16 <semin1116@gmail.com>
2022-06-26 14:57:31 +0900

    ch5

---
+++ ch5/devsns/devsns/my_settings.py
@@ -0,0 +1,12 @@
+mySECRET_KEY = 'django-insecure-#6-fj40mr95fzw(^56)5g%c(7y$xf$b7)e@=_v!+se!k!4ap@x'
+
+myDATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD':'1234',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
+    }
+}
